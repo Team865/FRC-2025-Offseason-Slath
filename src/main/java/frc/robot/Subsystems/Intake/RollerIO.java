a15a9b5e114ef0157c63623f24004773f928b3ea
TVA018 <thomas.vu018@gmail.com>
2025-04-15 16:45:53 -0400

    Add IO file for intake rollers

---
+++ src/main/java/frc/robot/Subsystems/Intake/RollerIO.java
@@ -0,0 +1,13 @@
+package frc.robot.Subsystems.Intake;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface RollerIO {
+    @AutoLog
+    public class RollerInputs {
+        public double volts;
+        public double velocity;
+    }
+
+    
+}
