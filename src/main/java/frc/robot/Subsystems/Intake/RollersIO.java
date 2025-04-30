e19be69f256dd4a276212ef477a18e15d52e6895
TVA018 <thomas.vu018@gmail.com>
2025-04-30 15:46:41 -0400

    Add additional properties for RollersIO AutoLog

--- src/main/java/frc/robot/Subsystems/Intake/RollersIO.java
+++ src/main/java/frc/robot/Subsystems/Intake/RollersIO.java
@@ -5,8 +5,10 @@ import org.littletonrobotics.junction.AutoLog;
 public interface RollersIO {
     @AutoLog
     public static class RollersIOInputs {
-        public double volts = 0.0;
-        public double velocity = 0.0;
+        public double appliedVolts = 0.0;
+        public double currentAmps = 0.0;
+        public double velocityRotPerSecond = 0.0;
+        public double positionRot = 0.0;
         public boolean connected = false;
     }
 
