73fedb513cf129e9b8e413eb93fb4519e120118c
TVA018 <thomas.vu018@gmail.com>
2025-05-01 21:54:58 -0400

    Add constants for intake sensors

--- src/main/java/frc/robot/Subsystems/Intake/IntakeConstants.java
+++ src/main/java/frc/robot/Subsystems/Intake/IntakeConstants.java
@@ -2,4 +2,8 @@ package frc.robot.Subsystems.Intake;
 
 public final class IntakeConstants {
     public static final int ROLLERS_ID = 2;
+
+    // Will be adjusted to be actually accurate later in-person
+    public static final int MIDDLE_SENSOR_MAX_DIST_MM = 50;
+    public static final int BOTTOM_SENSOR_MAX_DIST_MM = 50;
 }
