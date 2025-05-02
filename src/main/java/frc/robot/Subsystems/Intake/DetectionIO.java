4fe657f5ef8a452c5838904956daa77154c0882e
TVA018 <thomas.vu018@gmail.com>
2025-05-01 21:06:54 -0400

    Create Trigger for DetectionIO

--- src/main/java/frc/robot/Subsystems/Intake/DetectionIO.java
+++ src/main/java/frc/robot/Subsystems/Intake/DetectionIO.java
@@ -2,12 +2,16 @@ package frc.robot.Subsystems.Intake;
 
 import org.littletonrobotics.junction.AutoLog;
 
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
 public interface DetectionIO {
     @AutoLog
     public static class DetectionIOInputs {
         public boolean connected = false;
         public double distance = 0.0;
     }
+
+    Trigger objectDetected = new Trigger(null);
 
     public default void updateInputs(DetectionIOInputs inputs) {}
 }
