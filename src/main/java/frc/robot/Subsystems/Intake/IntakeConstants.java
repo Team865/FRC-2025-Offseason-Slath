7fddbdbbe407fd59e97ebcade3f8ab3d01cb303a
TVA018 <thomas.vu018@gmail.com>
2025-05-01 22:40:53 -0400

    Add device ids for sensors

--- src/main/java/frc/robot/Subsystems/Intake/IntakeConstants.java
+++ src/main/java/frc/robot/Subsystems/Intake/IntakeConstants.java
@@ -1,7 +1,12 @@
 package frc.robot.Subsystems.Intake;
 
 public final class IntakeConstants {
+    // Device IDs
     public static final int ROLLERS_ID = 2;
+
+    // Values are placeholder
+    public static final int MIDDLE_SENSOR_ID = 0;
+    public static final int BOTTOM_SENSOR_ID = 1;
 
     // Will be adjusted to be actually accurate later in-person
     public static final int MIDDLE_SENSOR_MAX_DIST_MM = 50;
