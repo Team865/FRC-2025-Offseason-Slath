d6330cda863c8adc502be0ca59b7ee2f3fd76ea8
TVA018 <thomas.vu018@gmail.com>
2025-05-01 21:53:25 -0400

    Change type of distance in DetectionIO to int

--- src/main/java/frc/robot/Subsystems/Intake/DetectionIO.java
+++ src/main/java/frc/robot/Subsystems/Intake/DetectionIO.java
@@ -1,16 +1,13 @@
 package frc.robot.Subsystems.Intake;
 
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import org.littletonrobotics.junction.AutoLog;
 
 public interface DetectionIO {
     @AutoLog
     public static class DetectionIOInputs {
         public boolean connected = false;
-        public double distance = 0.0;
+        public int distanceMM = 0;
     }
-
-    Trigger objectDetected = new Trigger(() -> true);
 
     public default void updateInputs(DetectionIOInputs inputs) {}
 }
