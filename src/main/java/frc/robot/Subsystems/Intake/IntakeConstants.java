ba138bc3c55621d4b6e77855ef7e9d3e7ca36e6c
TVA018 <thomas.vu018@gmail.com>
2025-05-10 14:30:00 -0400

    Spotlessapply

--- src/main/java/frc/robot/Subsystems/Intake/IntakeConstants.java
+++ src/main/java/frc/robot/Subsystems/Intake/IntakeConstants.java
@@ -8,7 +8,7 @@ public final class IntakeConstants {
     public static final int MIDDLE_SENSOR_ID = 13;
     public static final int BOTTOM_SENSOR_ID = 12;
 
-    //Accuratley tuned 
+    // Accuratley tuned
     public static final int MIDDLE_SENSOR_MAX_DIST_MM = 50;
     public static final int BOTTOM_SENSOR_MAX_DIST_MM = 110;
     public static final int COMBINED_MAX_DIST_MM = 160;
