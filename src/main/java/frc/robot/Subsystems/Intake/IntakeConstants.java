d0dc6212f020eab9366d75faadf2871b1b3c40f7
TVA018 <thomas.vu018@gmail.com>
2025-05-20 16:25:29 -0400

    Tune distances for intake, should be working now

--- src/main/java/frc/robot/Subsystems/Intake/IntakeConstants.java
+++ src/main/java/frc/robot/Subsystems/Intake/IntakeConstants.java
@@ -9,7 +9,7 @@ public final class IntakeConstants {
     public static final int BOTTOM_SENSOR_ID = 12;
 
     // Accuratley tuned
-    public static final int MIDDLE_SENSOR_MAX_DIST_MM = 50;
-    public static final int BOTTOM_SENSOR_MAX_DIST_MM = 110;
+    public static final int MIDDLE_SENSOR_MAX_DIST_MM = 150;
+    public static final int BOTTOM_SENSOR_MAX_DIST_MM = 270;
     public static final int COMBINED_MAX_DIST_MM = 160;
 }
