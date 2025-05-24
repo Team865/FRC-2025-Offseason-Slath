2952a3f059b6c5c2ebbbf5580dad2b9c338ba253
TVA018 <thomas.vu018@gmail.com>
2025-05-24 13:19:19 -0400

    Create elevator IO

---
+++ src/main/java/frc/robot/Subsystems/Elevator/ElevatorIO.java
@@ -0,0 +1,18 @@
+package frc.robot.Subsystems.Elevator;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ElevatorIO {
+    @AutoLog
+    public static class ElevatorIOInputs {
+        public double appliedVolts = 0.0;
+        public double currentAmps = 0.0;
+        public double velocityRotPerSecond = 0.0;
+        public double positionRot = 0.0;
+        public boolean connected = false;    
+    }
+
+    public default void setPID(int p, int i, int d){
+        // Do nothing
+    }
+}
