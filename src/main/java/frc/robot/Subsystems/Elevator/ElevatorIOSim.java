7d01357c36d89521288953e900d20629569df66f
TVA018 <thomas.vu018@gmail.com>
2025-05-24 14:33:23 -0400

    Setup simulation

--- src/main/java/frc/robot/Subsystems/Elevator/ElevatorIOSim.java
+++ src/main/java/frc/robot/Subsystems/Elevator/ElevatorIOSim.java
@@ -1,14 +1,20 @@
 package frc.robot.Subsystems.Elevator;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
 
 public class ElevatorIOSim implements ElevatorIO {
     private final PIDController pidController = new PIDController(0, 0, 0);
-    private double goal = 0.0;
+    private double appliedVolts = 0.0;
+    private double goalInches = 0.0;
+
+    private ElevatorSim elevatorSim = new ElevatorSim(4.62, 0.07, DCMotor.getKrakenX60(2), Units.inchesToMeters(0), Units.inchesToMeters(29), true, 0);
 
     @Override
     public void setGoal(double goal) {
-        this.goal = goal;
+        this.goalInches = goal;
     }
 
     @Override
@@ -20,7 +26,13 @@ public class ElevatorIOSim implements ElevatorIO {
 
     @Override
     public void updateInputs(ElevatorIOInputs inputs) {
-        // TODO Auto-generated method stub
-        ElevatorIO.super.updateInputs(inputs);
+        appliedVolts = pidController.calculate(inputs.positionInches, goalInches);
+        elevatorSim.setInputVoltage(appliedVolts);
+        elevatorSim.update(0.02);
+
+        inputs.appliedVolts = appliedVolts;
+        inputs.currentAmps = elevatorSim.getCurrentDrawAmps();
+        inputs.positionInches = Units.metersToInches(elevatorSim.getPositionMeters());
+        inputs.velocityInchesPerSecond = Units.metersToInches(elevatorSim.getVelocityMetersPerSecond());
     }
 }
